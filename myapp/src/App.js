f8ed143dc6bccbab286c1d17ab07e1e9abc6ef23
jisunnyyy <wltjs1211@gmail.com>
2023-03-25 07:32:22 +0900

    first release

---
+++ myapp/src/App.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>
+        The Coins! {loading ? "" : `(${coins.length})`}
+        {loading ? (
+          <strong> "Loading..."</strong>
+        ) : (
+          <ul>
+            {coins.map((coin) => (
+              <li>
+                {coin.name} ({coin.symbol}) :${coin.quotes.USD.price} USD
+              </li>
+            ))}
+          </ul>
+        )}
+      </h1>
+    </div>
+  );
+}
+
+export default App;
